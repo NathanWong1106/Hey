75f52c65a7d555ac8f95d658ed5c538d78cbdadd
duccie42 <arwinfong@gmail.com>
2021-10-17 06:28:55 -0400

    fix

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React, {useState, useEffect} from "react";
+import socketIOClient from "socket.io-client"
+const ENDPOINT = "http://localhost:3000"
+
+
+function App() {
+  const [response, setResponse] = useState("");
+  const [cameraStates, setCameraStates] = useState(new Map());
+
+  useEffect(() =>{ 
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("connect", () => {
+      console.log("connected")
+    })
+    socket.on('ping', data => {
+      console.log(new Map(JSON.parse(data.state)))
+      setResponse(data);
+    });
+
+  }, []);
+  return (
+    <div className="App">
+      <h1> Hello</h1>
+      <script src="/socket.io-client/dist/socket.io.js"></script>
+    </div>
+
+  );
+}
+
+export default App;
