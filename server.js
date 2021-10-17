97e020841f0aa2fa1425b3ee0c6346f1501d9bfc
Eric <fcweric@gmail.com>
2021-10-17 04:02:59 -0400

    Removal of emp ping

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const net = require('net');
 const fs = require('fs');
 
 var cam_state = new Map();
+var emp_list = []
 
 /* static files
 app.get('/', (req, res) => {
@@ -21,23 +22,13 @@ var io = socket(server) // socket setup
 
 io.on('connection', (socket) => {
     console.log('User connected');
-    id = "Employee 1"; // payload.id from cameras
-    state = true; // payload.state from cameras
-    if(id.includes("Camera")){
-        socket.join("cam"); // camera room
-        room = "cam"
-        
-    } else if(id.includes("Employee")){
-        socket.join("emp"); // employee room
-        room = "emp"
-    }
-    socket.on("receive state", () => { // payload
-        cam_state.set(id, state);
+    socket.on('cam_update', () => {
+            id = cam.id;
+            state = cam.state;
+            cam_state.set(id, state);
     })
     console.log(cam_state);
-    socket.on("receive ping", () => { // from employees, 5-sec intervals
-        socket.to("emp").emit(cam_state) // Broadcast map
-    });    
+    setInterval(() => io.emit('ping', cam_state), 5000); // broadcast map at 5 sec interval
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
