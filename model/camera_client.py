3c21ba4e3503bde1c24905a23c0a8def6b56cae4
Nathan Wong <56657800+NathanWong1106@users.noreply.github.com>
2021-10-17 04:03:38 -0400

    camera kinda works

---
+++ model/camera_client.py
@@ -0,0 +1,39 @@
+import socketio
+import cv2
+from detection import get_landmark_results, annotate_img, predict
+import time
+
+EMIT_FREQUENCY = 5 #how long (in seconds) before a ping is sent to server
+
+def main():
+    sio = socketio.Client()
+    sio.connect("http://localhost:3000")
+
+    capture = cv2.VideoCapture(0)
+
+    last_emit = time.time()
+
+    while True:
+        success, img = capture.read()
+        img = cv2.flip(img, 1)
+        key = cv2.waitKey(1)
+
+        if key == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+        results = get_landmark_results(img)
+        if results.pose_landmarks != None:
+            img = annotate_img(img, results)
+            is_raised = predict(results)
+        else:
+            is_raised = False
+
+        cv2.imshow("Camera", img)
+
+        if time.time() - last_emit > EMIT_FREQUENCY:
+            sio.emit("cam_update", {"id": 1, "state": is_raised})
+            last_emit = time.time()
+
+if __name__ == "__main__":
+    main()
--- model/detection.py
+++ model/detection.py
@@ -15,38 +15,23 @@ mp_pose = mp.solutions.pose
 pose = mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, model_complexity=2)
 
 mp_drawing = mp.solutions.drawing_utils 
+model = keras.models.load_model("./model/trained_model")
 
-def main():
-    model = keras.models.load_model("./model/trained_model")
-    capture = cv2.VideoCapture(0)
-
-    while True:
-        success, img = capture.read()
-        img = cv2.flip(img, 1)
-        key = cv2.waitKey(1)
-
-        img, res = process_frame(img)
-        
-        if res.pose_landmarks != None:
-            print(model.predict(np.array(get_standard_data(res)).reshape(1,66)))
-
-        cv2.imshow("Test", img)
-
-        if key == ord('q'):
-            cv2.destroyAllWindows()
-            break
-
-def process_frame(img: np.ndarray):
+def get_landmark_results(img):
     converted = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = pose.process(converted)
+    return results
+
+def annotate_img(img, results):
+    converted = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     annotated_image = img.copy()
     mp_drawing.draw_landmarks(
         annotated_image,
         results.pose_landmarks,
         mp_pose.POSE_CONNECTIONS)
-    return (annotated_image, results)
+    return annotated_image
 
-if __name__ == "__main__":
-    main()
+def predict(results):
+    return (model.predict(np.array(get_standard_data(results)).reshape(1,66))[0][0] < 0.4)
     
